5897936327f36a381ea740a264e9d29c5d7bea2a
Andrew Chen <andrewchen.c44@gmail.com>
2017-12-13 17:52:46 -0800

    Create basic functions

--- app/elastic.js
+++ app/elastic.js
@@ -5,17 +5,59 @@ const client = new elasticsearch.Client({
   log: 'trace'
 })
 
+const indexName = 'videos'
+
+function initIndex() {
+    return client.indices.create({
+    index: indexName
+  });
+};
+
+function initMapping() {
+  return client.indices.putMapping({
+    index: indexName,
+    type: 'document',
+    body: {
+      properties: {
+        url: {type: 'string'},
+      }
+    }
+  })
+}
+
+function addDocument(document) {
+  return client.index({
+    index: indexName,
+    type: 'document',
+    body: {
+      title:document.title,
+    }
+  })
+}
+
+// initIndex().then( (results) => {
+//   console.log(results);
+// })
+
+addDocument({title: 'test2'}).then( (results) => {
+  console.log(results);
+});
+
 client.ping(
-  {
-    requestTimeout: 30000
-  },
+  {requestTimeout: 30000},
   function(error) {
     if (error) {
       console.error("elasticsearch cluster is down!");
     } else {
-      console.log("All is well");
+      console.log("Elastic search connection is running.");
     }
   }
 );
+
+
+
+
+
+
 
 module.exports = client;
