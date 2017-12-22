a69b7163a37b1b594912b799231451b8e8e8665c
Andrew Chen <andrewchen.c44@gmail.com>
2017-12-21 17:16:51 -0800

    Clean up code

--- app/queryElastic.js
+++ app/queryElastic.js
@@ -8,35 +8,16 @@ const search = (query, callback) => {
     query: {
       match: {"title": query}
     },
-  }
+  },
     })
     .then(
       function(body) {
-        callback(body)
+        callback(body);
       },
       function(error) {
-        console.trace(error.message);
+        console.log(error.message);
       }
     );
 }
-
-const query = function(count) {
-  for (let i = 0; i < 200; i++) {
-    search('cats');
-    count--;
-    console.log(count);
-    if (count === 0) {
-      return;
-    }
-    if (i === 199) {
-      search().then(() => {
-        count--;
-        console.log(count);
-        query(count);
-      });
-    }
-  }
-};
-
 
 module.exports = search;
