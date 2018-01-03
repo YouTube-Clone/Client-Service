0db7d488d3be57be20bbdd892e93da8db464a83b
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-02 17:28:01 -0800

    Fix tests

--- test/eventLogSpec.js
+++ test/eventLogSpec.js
@@ -9,19 +9,19 @@ describe('Event Log Enpoint Tests', () => {
 
   it('should return a 404 for get reguests to the /video/:video_id/log endpoint', (done) => {
     supertest(server)
-      .get('/video/videoId/log')
+      .get('/videos/videoId/log')
       .expect(404, done)
   })
 
   it('Should return a 200 for a post request sent to the /video/video_id/log endpoint', (done) => {
     supertest(server)
-      .post('/video/videoId/log')
+      .post('/videos/videoId/log')
       .expect(200, done)
   })
 
   it('Should return a 404 for a patch request sent to the /video/video_id/log endpoint', (done) => {
     supertest(server)
-      .patch('/video/videoId/log')
+      .patch('/videos/videoId/log')
       .expect(404, done)
   })
 
